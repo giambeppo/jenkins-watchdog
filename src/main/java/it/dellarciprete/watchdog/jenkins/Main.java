1a38a2184c04da17a0316c5755674e30f107daa0
giambeppo <lorenzo.dellarciprete@gmail.com>
2017-05-31 17:34:18 +0200

    Fixed handling of initialization issues (e.g. missing mandatory configuration)

--- src/main/java/it/dellarciprete/watchdog/jenkins/Main.java
+++ src/main/java/it/dellarciprete/watchdog/jenkins/Main.java
@@ -1,15 +1,22 @@
 package it.dellarciprete.watchdog.jenkins;
 
+import java.util.logging.Level;
+
 public class Main {
 
-  public static void main(String[] args) throws InterruptedException, WatchDogException {
-    WatchDogTrayIcon.initializeTrayIcon();
-    String propertiesFile = System.getProperty("propertiesFile", "config.properties");
-    Configuration config = new Configuration(propertiesFile);
-    JenkinsClient jenkinsClient = new JenkinsClient(config);
-    LatestFailure latestFailure = new LatestFailure(config);
-    MailClient mailClient = new MailClient(config);
-    new WatchDog(config, jenkinsClient, latestFailure, mailClient).startControlLoop();
+  public static void main(String[] args) {
+    try {
+      WatchDogTrayIcon.initializeTrayIcon();
+      String propertiesFile = System.getProperty("propertiesFile", "config.properties");
+      Configuration config = new Configuration(propertiesFile);
+      JenkinsClient jenkinsClient = new JenkinsClient(config);
+      LatestFailure latestFailure = new LatestFailure(config);
+      MailClient mailClient = new MailClient(config);
+      new WatchDog(config, jenkinsClient, latestFailure, mailClient).startControlLoop();
+    } catch (WatchDogException e) {
+      WatchDogLogger.get().log(Level.SEVERE, "Initialization error", e);
+      WatchDogLogger.finalizeLogger();
+    }
   }
 
 }
